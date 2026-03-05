dac8b3f261904dd9ffea82d36749e4b18bdc652e
Ruchi <ruchimalik11@gmail.com>
2026-03-05 23:07:12 +0000

    changes to SpecUtil

--- src/test/java/com/api/utils/SpecUtil.java
+++ src/test/java/com/api/utils/SpecUtil.java
@@ -33,12 +33,12 @@ public class SpecUtil {
 	}
 
 	//POST/PUT/PATCH
-	public static RequestSpecification requestSpec(UserCredentials userCredentials) {
+	public static RequestSpecification requestSpec(Object payload) {
 		//to takecare of the common request sections(methods)
 	RequestSpecification request=new RequestSpecBuilder().setBaseUri(ConfigManager.getProperty("BASE_URI"))
 		.setContentType(ContentType.JSON)
 		.setAccept(ContentType.JSON)
-		.setBody(userCredentials)
+		.setBody(payload)
 		.log(LogDetail.URI)
 		.log(LogDetail.METHOD)
 		.log(LogDetail.HEADERS)
