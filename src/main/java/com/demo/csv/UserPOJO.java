6f0674ae3d101bd0c8d763061eb7985c2e451c89
Ruchi <ruchimalik11@gmail.com>
2026-03-13 15:12:19 +0000

    Practiced mapping the CSV file to POJO

---
+++ src/main/java/com/demo/csv/UserPOJO.java
@@ -0,0 +1,38 @@
+package com.demo.csv;
+
+public class UserPOJO {
+	private String username;
+	
+	public UserPOJO() {
+		
+	}
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String toString() {
+		return "UserPOJO [username=" + username + ", password=" + password + "]";
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	private String password;
+
+	public UserPOJO(String username, String password) {
+		super();
+		this.username = username;
+		this.password = password;
+	}
+
+}
