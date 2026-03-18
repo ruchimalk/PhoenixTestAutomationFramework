80e289b14d7d66e5b6b3c8afccc9fed16508f2fd
Ruchi <ruchimalik11@gmail.com>
2026-03-18 14:38:03 +0000

    Completed requirement 8-Data driven testing with csv 1. LoginAPIDataDrivenTest 2. CreateJobAPIDataDrivenTest

--- src/test/java/com/api/utils/CSVReaderUtil.java
+++ src/test/java/com/api/utils/CSVReaderUtil.java
@@ -21,18 +21,18 @@ public class CSVReaderUtil {
 		//singleton class constructors are private
 	}
 	
-public static Iterator<UserBean> loadCSV(String pathOfCSVFile) {
+public static <T> Iterator<T> loadCSV(String pathOfCSVFile, Class<T> bean) {
 
 		
 		InputStream is=Thread.currentThread().getContextClassLoader().getSystemResourceAsStream(pathOfCSVFile);
 		InputStreamReader isr= new InputStreamReader(is);
 		CSVReader csvReader= new CSVReader(isr);
-		CsvToBean<UserBean> csvToBean= new CsvToBeanBuilder(csvReader)
-				.withType(UserBean.class)
+		CsvToBean<T> csvToBean= new CsvToBeanBuilder(csvReader)
+				.withType(bean)
 				.withIgnoreEmptyLine(true)
 				.build();
-	     	List<UserBean> userList=csvToBean.parse();
-	     return	userList.iterator();
+	     	List<T> list=csvToBean.parse();
+	     return	list.iterator();
 	
 		
 	}
